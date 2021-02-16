fbe923109bb652c709d3568f87e031332c8a0f9b
Jose <jose23scl@gmail.com>
2021-02-16 10:34:24 -0300

    Se logra hacer el insersectionObservation en JS!

--- script.js
+++ script.js
@@ -1,34 +1,11 @@
-/* const box = document.querySelectorAll('.timeline-graphs');
-
-observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.intersectionRatio > 0) {
-      entry.target.classList.add('isVisible');
-    } else {
-      entry.target.classList.remove('isVisible');
-    }
-  });
-});
-
-box.forEach(box => {
-  observer.observe(box);
-}); */
-
-
-
-
-
 const elementsToExpand = document.querySelectorAll('.timeline-graphs');
 
 let expansionObserver = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.intersectionRatio > 0) {
-      console.log('Yep');
-      //entry.target.style.animation = `expansion 2s ease-in-out`;
-
+      entry.target.classList.add('isVisible');
     } else {
-      // entry.target.style.animation = "none";
-      console.log('Nope')
+      entry.target.classList.remove('isVisible');
     }
   });
 
@@ -38,4 +15,3 @@ elementsToExpand.forEach((element) => {
   expansionObserver.observe(element);
 });
 
-console.log('Hi');
